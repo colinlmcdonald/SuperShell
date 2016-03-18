f6528d303ff9ba06ee7861c06edd36675cfb07f7
Colin McDonald <mickydwow@gmail.com>
2016-03-17 17:17:09 -0700

    Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './public/src/index'
+    './src/index'
   ],
 
   output: {
