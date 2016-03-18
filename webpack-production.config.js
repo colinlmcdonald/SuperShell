cd53e77a7535172004d0294c3d5aa9208c7f5ee2
Colin McDonald <mickydwow@gmail.com>
2016-03-17 17:13:16 -0700

    Heroku

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './src/index'
+    './public/src/index.js'
   ],
   output: {
     path: path.join(__dirname, 'public'),
