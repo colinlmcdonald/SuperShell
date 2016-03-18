5f997f80f2b41af9d5a4f75e5c82d3bf6bf4490c
Colin McDonald <mickydwow@gmail.com>
2016-03-17 21:20:30 -0700

    yup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import Character from './Character'
 
 render(<Character />, document.getElementById('app'));
-=======
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Character from './Character';
-
-
-ReactDOM.render(<Character />, document.getElementById('app'));
->>>>>>> a79172a8b89027d926679caf4bb96778654b8265
