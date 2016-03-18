2750865962c228ac8454a72c844cde0df2ca4558
Colin McDonald <mickydwow@gmail.com>
2016-03-17 20:32:18 -0700

    Heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import 'babel-core/polyfill';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Character from './Character';
+
 
 ReactDOM.render(<Character />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './index'
+    './src/index'
   ],
 
   output: {
@@ -29,7 +29,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/,
         loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015'],
-        include: path.join(__dirname),
+        include: path.join(__dirname, 'src'),
       },
       { test: /\.scss?$/,
         loader: 'style!css!sass',
