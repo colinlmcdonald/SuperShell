e630ba7efbddfe43cabdbd4d94e0ba7c1b0d709b
Colin McDonald <mickydwow@gmail.com>
2016-03-17 20:28:39 -0700

    heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import Character from './Character'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Character from './Character';
 
 ReactDOM.render(<Character />, document.getElementById('app'));
